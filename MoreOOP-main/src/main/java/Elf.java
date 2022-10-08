1f6174c44fce6ae210a6a90ad9f8e0c9c3cd6917
Arsenii6666 <92683180+Arsenii6666@users.noreply.github.com>
2022-10-08 19:16:28 +0300

    Delete MoreOOP-main directory

--- MoreOOP-main/src/main/java/Elf.java
+++
@@ -1,9 +0,0 @@
-
-public class Elf extends Character{
-    Elf(){
-        setHp(10);
-        setPower(10);
-    }
-    @Override
-    public void kick(Character c) {Kicks.elfKick(this, c);}
-}
--- MoreOOP-main/src/main/java/King.java
+++
@@ -1,9 +0,0 @@
-public class King extends Noble {
-    King(){
-        setMaxhp(15);
-        setMinhp(5);
-        setMaxpow(15);
-        setMinpow(5);
-        Generator();
-    }
-}
